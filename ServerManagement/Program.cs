f1244c7682fef189cdaeb7376590ba5deaf05769
Neil <neil.bhasme@gmail.com>
2025-01-30 12:39:30 +0200

    Navigation and handled related error

--- ServerManagement/Program.cs
+++ ServerManagement/Program.cs
@@ -3,7 +3,9 @@ using ServerManagement.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+//builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -20,6 +22,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+//app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
